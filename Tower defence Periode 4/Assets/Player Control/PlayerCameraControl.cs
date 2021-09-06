a129ab964d284c4c0b350820e07db47193fd63ed
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2021-09-06 16:24:27 +0200

    camera fix (FINALLY!!!)

--- Tower defence Periode 4/Assets/Player Control/PlayerCameraControl.cs
+++ Tower defence Periode 4/Assets/Player Control/PlayerCameraControl.cs
@@ -6,28 +6,17 @@ using UnityEngine.InputSystem;
 public class PlayerCameraControl : MonoBehaviour
 {
     public float turnSpeed;
-    private Quaternion _cameraTargetRot;
+    Vector3 angles;
 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     void OnLook(InputValue lookValue)
     {
         Vector2 lookVector = lookValue.Get<Vector2>();
-        _cameraTargetRot *= Quaternion.Euler(-lookVector.y * turnSpeed, 0f, 0f);
-        _cameraTargetRot = LockCameraMovement(_cameraTargetRot);
-        transform.rotation = _cameraTargetRot;
-    }
-    private Quaternion LockCameraMovement(Quaternion q)
-    {
-        q.x /= q.w;
-        q.y /= q.w;
-        q.z /= q.w;
-        q.w = 1.0f;
-
-        var angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
-
-        angleX = Mathf.Clamp(angleX, -90, 90);
-
-        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
-
-        return q;
+        angles.x -= lookVector.y * turnSpeed;
+        angles.x = Mathf.Clamp(angles.x, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(angles);
     }
 }
