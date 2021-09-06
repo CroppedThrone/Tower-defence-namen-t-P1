91034089b48c639271a4f57e9c0ead4fe8e5d9ca
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2021-09-06 14:02:27 +0200

    added main menu scene

--- Tower defence Periode 4/Assets/Player Control/PlayerCameraControl.cs
+++ Tower defence Periode 4/Assets/Player Control/PlayerCameraControl.cs
@@ -6,12 +6,28 @@ using UnityEngine.InputSystem;
 public class PlayerCameraControl : MonoBehaviour
 {
     public float turnSpeed;
+    private Quaternion _cameraTargetRot;
 
     void OnLook(InputValue lookValue)
     {
         Vector2 lookVector = lookValue.Get<Vector2>();
-        transform.Rotate(-lookVector.y * turnSpeed, 0, 0);
-        //Vector3 finalRotation = new Vector3(Mathf.Clamp(transform.eulerAngles.x, 0, 180), transform.eulerAngles.y, transform.eulerAngles.z);
-        //transform.eulerAngles = finalRotation;
+        _cameraTargetRot *= Quaternion.Euler(-lookVector.y * turnSpeed, 0f, 0f);
+        _cameraTargetRot = LockCameraMovement(_cameraTargetRot);
+        transform.rotation = _cameraTargetRot;
+    }
+    private Quaternion LockCameraMovement(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        var angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, -90, 90);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
     }
 }
