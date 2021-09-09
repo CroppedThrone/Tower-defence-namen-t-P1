9f955332055367c717a4708d2cb054e74552544f
Beau00 <70371863+Beau00@users.noreply.github.com>
2021-09-09 14:52:38 +0200

    ui changes
    pijl toetsen werken nu

---
+++ Tower defence Periode 4/Assets/Main Menu/testttttttt.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testttttttt : MonoBehaviour
+{
+
+
+
+    public void pront()
+    {
+        print("moiiiii");
+    }
+
+    public void pront1()
+    {
+        print("henlo");
+    }
+}
