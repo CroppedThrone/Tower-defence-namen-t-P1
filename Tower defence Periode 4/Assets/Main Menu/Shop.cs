bf8c3b3766934ff5f794a63bea3da5eba9566c33
Beau00 <70371863+Beau00@users.noreply.github.com>
2021-09-14 16:26:53 +0200

    Ui menu with shop dropdown

--- Tower defence Periode 4/Assets/Main Menu/Shop.cs
+++ Tower defence Periode 4/Assets/Main Menu/Shop.cs
@@ -2,17 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class Shop : MonoBehaviour
 {
-   public GameObject dropdown;
-    public GameObject button;
+    public GameObject starter;
+    public GameObject elite;
+    public GameObject sniper;
+    public Dropdown shop;
+    public GameObject currentSelected;
 
-    // Update is called once per frame
-    public void Dropdown()
+    
+    void Update()
     {
-        dropdown.SetActive(true);
-        button.SetActive(false);
+
+
+        currentSelected = EventSystem.current.currentSelectedGameObject;
+        if(currentSelected.name == "Item 1: Starter Turret")
+        {
+                starter.SetActive(true);
+        }
+        else
+        {
+            starter.SetActive(false);
+        }
+
+        if (currentSelected.name == "Item 2: Elite Turret")
+        {
+            elite.SetActive(true);
+        }
+        else
+        {
+            elite.SetActive(false);
+        }
+        
+        if (currentSelected.name == "Item 3: Sniper Turret")
+        {
+            sniper.SetActive(true);
+        }
+        else
+        {
+            sniper.SetActive(false);
+        }
+
 
     }
+
+
+
+    
+        
+        
+      
+        
+
+    
+    
+         
+    
+
+
+
+
 }
