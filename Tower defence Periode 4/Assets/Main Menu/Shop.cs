d477e51f8211512d94feb48366c40d0956e18c2e
Beau00 <70371863+Beau00@users.noreply.github.com>
2021-09-16 16:57:20 +0200

    ui fixie

--- Tower defence Periode 4/Assets/Main Menu/Shop.cs
+++ Tower defence Periode 4/Assets/Main Menu/Shop.cs
@@ -12,19 +12,55 @@ public class Shop : MonoBehaviour
     public GameObject sniper;
     public GameObject missle;
     public GameObject machine;
-    public Dropdown shop;
+    public GameObject drop;
+    public int lastPressed = 0;
+    GameObject currentSelected;
 
-
+    public void Drop()
+    {
+        lastPressed++;
+        if (lastPressed == 1)
+        {
+            drop.SetActive(true);
+        }
+        else
+        {
+            lastPressed = 0;
+            drop.SetActive(false);
+        }
+    }
 
     void Update()
     {
+        currentSelected = EventSystem.current.currentSelectedGameObject;
 
-      
-      
-
-
-
+        if (currentSelected.name == "Starter Turret")
+        {
+            starter.SetActive(true);
+        }
+        else
+        {
+            starter.SetActive(false);
+        }
+        if (currentSelected.name == "Elite Turret")
+        {
+            elite.SetActive(true);
+        }
+        else
+        {
+            elite.SetActive(false);
+        }
+        if (currentSelected.name == "Machine Gun Turret")
+        {
+            machine.SetActive(true);
+        }
+        else
+        {
+           machine.SetActive(false);
+        }
+        
     }
+
 
 
 
