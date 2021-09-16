413f9f1c6d63292a041054c69aa55f410abee78e
Beau00 <70371863+Beau00@users.noreply.github.com>
2021-09-16 16:03:33 +0200

    Revert "Revert "unity update not done buggy""
    This reverts commit bdb63539155bed2962d2e61016ce579a74d4bd30.

---
+++ Tower defence Periode 4/Assets/Main Menu/Confirmscreen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Confirmscreen : MonoBehaviour
+{
+    public GameObject conff;
+    public void conf()
+    {
+        conff.SetActive(true);
+    }
+
+
+}
