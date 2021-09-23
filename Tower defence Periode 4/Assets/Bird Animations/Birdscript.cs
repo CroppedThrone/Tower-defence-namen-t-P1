39a5d2fef3c4ddc61187f9957c44c2af7e624fc0
Rubenstienstra <78020104+Rubenstienstra@users.noreply.github.com>
2021-09-23 15:39:06 +0200

    Smart bird.Y 1/2
    ik heb tot nu toe de y-level af. Je kan de vogel script overal neer zetten en dan gaat hij automatisch als een vogel bewegen.

--- Tower defence Periode 4/Assets/Bird Animations/Birdscript.cs
+++ Tower defence Periode 4/Assets/Bird Animations/Birdscript.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class Birdscript : MonoBehaviour
 {
     public float birdSpeed;
-    public float birdLevel;
+    public Vector3 birdLevel;
     public Vector3 move;
     public bool dumbBird;
 
-    public Vector3 positionBird;
+    public bool smartBird;
+    public Vector3 positionBirdMin;
+    public Vector3 positionBirdPlus;
 
     // Start is called before the first frame update
     void Start()
     {
-        positionBird.y = transform.position.y;
-        positionBird.z = transform.position.z;
-        positionBird.x = transform.position.x;
+
+        smartBird = true;
+
+        positionBirdMin.y = transform.position.y;
+        positionBirdMin.z = transform.position.z;
+        positionBirdMin.x = transform.position.x;
+        positionBirdPlus.y = transform.position.y;
+        positionBirdPlus.z = transform.position.z;
+        positionBirdPlus.x = transform.position.x;
+
+        positionBirdPlus.y++;
+        positionBirdPlus.z++;
+        positionBirdPlus.x++;
+        positionBirdMin.y--;
+        positionBirdMin.z--;
+        positionBirdMin.x--;
     }
 
     // Update is called once per frame
@@ -24,21 +39,34 @@ public class Birdscript : MonoBehaviour
     {
 
         transform.Translate(move * birdSpeed * Time.deltaTime);
-        move.y = birdLevel;
+        move.y = birdLevel.y;
 
         if (dumbBird == true)
         {
             if (transform.position.y >= 9)
             {
-                birdLevel = -0.625f;
+                birdLevel.y = -0.625f;
             }
             if (transform.position.y <= 7.5f)
             {
-                birdLevel = 0.5f;
+                birdLevel.y = 0.5f;
             }
         }
 
-
-
+        if (smartBird == true)
+        {
+            if (transform.position.y >= positionBirdPlus.y)
+            {
+                birdLevel.y = -0.625f;
+            }
+            if (transform.position.y <= positionBirdMin.y)
+            {
+                birdLevel.y = 0.5f;
+            }
+            if (transform.position.x >= positionBirdPlus.y)
+            {
+                birdLevel.x = 0.25f;
+            }
+        }
     }
 }
