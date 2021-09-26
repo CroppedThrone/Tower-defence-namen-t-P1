90f410496f9c9b3518857a852f255beeb0f8914c
Beau00 <70371863+Beau00@users.noreply.github.com>
2021-09-26 14:51:23 +0200

    Main menu explosion part and smoke part drop

---
+++ Tower defence Periode 4/Assets/Main Menu/Scripts/Exitbutton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exitbutton : MonoBehaviour
+{
+ 
+    public void ExitButton()
+    {
+        Application.Quit();
+        print("quit");
+    }
+}
