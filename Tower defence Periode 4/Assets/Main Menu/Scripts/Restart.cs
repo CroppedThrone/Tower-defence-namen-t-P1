3c7bf502a00c51d2c5bf6d69fa7693767835c191
Beau00 <70371863+Beau00@users.noreply.github.com>
2021-10-11 17:02:16 +0200

    death

---
+++ Tower defence Periode 4/Assets/Main Menu/Scripts/Restart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+   
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
+    public void ButtonRestart()
+    {
+        SceneManager.LoadScene(1);
+        
+    }
+}
