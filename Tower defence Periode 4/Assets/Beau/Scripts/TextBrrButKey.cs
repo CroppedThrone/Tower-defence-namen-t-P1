8a999e412dc15d4616b32179088aa09f1ee0949a
Beau00 <70371863+Beau00@users.noreply.github.com>
2021-10-28 00:40:04 +0200

    fix

--- Tower defence Periode 4/Assets/Beau/Scripts/TextBrrButKey.cs
+++ Tower defence Periode 4/Assets/Beau/Scripts/TextBrrButKey.cs
@@ -10,15 +10,11 @@ public class TextBrrButKey : MonoBehaviour, ISelectHandler
 
     public Text theText;
     
-    public void OnselectEnter(PointerEventData eventData)
+    public void Onselect()
     {
         theText.color = Color.white;
     }
-    public void OnselectExit(PointerEventData eventData)
-    {
-        theText.color = new Color32(74, 74, 73, 255);
-    }
-
+    
 
 }
 
